e20b56052f41a1ab763e103b324a9cb1f5ee07c9
ISHANT-AG <agarwalishant690@gmail.com>
2026-04-21 11:52:40 +0530

    ishant

--- script.js
+++ script.js
@@ -17,3 +17,19 @@ console.log("a = ")
 let cond1 = a > b;
 let cond2 = a == 6;
 console.log("cond1 && cond2 = ", cond1 && cond2);
+
+//conditional statement
+ let age = 20;
+
+if (age >= 25) {
+    console.log("you can purchase a bike");
+}
+if (age < 25) {
+    console.log("you can't");
+}
+
+if (age >= 25) {
+    console.log("hii");
+} else {
+    console.log("byy");
+}
