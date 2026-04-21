fd3a1849587dcf30cf64b55ca1e6b038251fb569
ISHANT-AG <agarwalishant690@gmail.com>
2026-04-21 07:51:47 +0530

    ishant day 2

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("hello world");
+// this is a comment okay
+// arithmetic Operator
+let a = 5;
+let b = 2;
+
+console.log("a +b = ", a + b);
+console.log("a-b =", a - b );
+console.log("a*b =", a * b );
+console.log("a/b =", a / b );
+console.log("a%b =", a % b );
