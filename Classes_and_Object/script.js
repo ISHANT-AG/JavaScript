379d325ac65af7a5e00bf7416912226d6f88b26e
ISHANT-AG <agarwalishant690@gmail.com>
2026-05-02 16:32:54 +0530

    ishant

--- Classes_and_Object/script.js
+++ Classes_and_Object/script.js
@@ -37,3 +37,88 @@ class KIA_Car {
 
 let fortuner = new KIA_Car();
 let LEXUS = new KIA_Car();
+
+
+//inheritance = passing down properties from parent class to child class
+
+class parent {
+    hello () {
+        console.log("hello");
+    }
+}
+
+
+class Child extends parent {
+    
+}
+
+let obj = new Child();
+
+
+class person {
+    eat() {
+        console.log("Eat")
+    }
+    sleep() {
+        console.log("sleep")
+    }
+    run() {
+        console.log("run")
+    }
+}
+
+class engineer extends person {
+    constructor(branch) {
+        super(); // to invoke parent class constructor
+        this.branch = branch;
+    }
+    work() {
+        console.log("solve problem,build something");
+    }
+}
+
+let ishantobj = new engineer("Chemical eng");
+
+//super keyword
+
+ class user {
+    constructor(name,email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    viewdata() {
+        console.log("data = ",DATA);
+    }
+ }
+
+ let student1 = new user("ishant","agarwalishant690@gmail.com");
+ let student2 = new user("bittu","bittu408@gmail.com");
+
+ class admin extends user {
+    constructor(name,email) {
+        super(name,email);
+    }
+    editdata() {
+        DATA = "Some new value";
+    }
+ }
+
+ let admin1 = new admin("ishant","agarwalishant");
+
+
+ //Error Handling
+ let a = 5;
+ let b = 10;
+ console.log("A + B = ", a + b );
+ console.log("A + B = ", a + b );
+ console.log("A + B = ", a + b );
+ console.log("A + B = ", a + b );
+ try {
+    console.log("A + B = ", a + b);
+ } catch (err) {
+    console.log("Err");
+ }
+
+ console.log("A + B = ", a + b );
+ console.log("A + B = ", a + b );
