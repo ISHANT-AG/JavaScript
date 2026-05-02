1eee82964238802c7ce71750acc457b299f0714d
ISHANT-AG <agarwalishant690@gmail.com>
2026-05-02 21:59:46 +0530

    ishant

--- CallBack/script.js
+++ CallBack/script.js
@@ -7,6 +7,36 @@ function hello () {
 
 setTimeout(hello,2000);
 
-//this will not wait asychronous programming
+//this will not wait #asychronous programming
 console.log("three");
 console.log("four");
+
+//callback
+function sum (a,b) {
+    console.log (a + b);
+}
+
+function calculator(a, b, sumcallback) {
+    sumcallback(a,b);
+}
+
+calculator(1,2,sum);
+
+//callback_hell =>> pyramid of loop (nested loop of looppppppppppppps)
+
+//ok-ji-ok
+
+function getdata(DataID,getNextData) {//2s
+    setTimeout(() => {
+         console.log("DATA =", DataID);
+         if (getNextData) {
+            getNextData();
+         }
+    }, 2000);
+}
+//callback Hell -------> pyramid doom
+ getdata(1, () => {
+    getdata(2, () => {
+        getdata (3);
+    });
+ });
